9f0ec3bbe2ddfeb72c0151cfe71345a0d8fee1ad
MigueldeNobrega <>
2024-12-18 11:42:32 +0100

    CodigoParaMovil

--- Assets/Scripts/PalaMovement.cs
+++ Assets/Scripts/PalaMovement.cs
@@ -21,15 +21,12 @@ public class PalaMovement : MonoBehaviour
        
         rb = GetComponent<Rigidbody2D>();    
         
-        
-
-
     }
 
     // Update is called once per frame
     void Update()
     {
-
+       /* 
         if (PalaIzquierda)
         {
             y = Input.GetAxisRaw("Vertical");
@@ -39,12 +36,25 @@ public class PalaMovement : MonoBehaviour
         {
             y = Input.GetAxisRaw("Vertical2");
         }
-
-
+        
         
         transform.Translate(new Vector2(0, y) * speed * Time.deltaTime);
-       
+       */
 
+        //Para MOVIL:
+        foreach(Touch touch in Input.touches)
+        {
+            if(touch.phase == TouchPhase.Moved)
+            {
+                //touch.position
+                transform.position = new Vector3(transform.position.x, touch.position.y, transform.position.z);
+            }
+            else if(touch.phase == TouchPhase.Ended){
+
+                GetComponent<SpriteRenderer>().color = new Color(Random.Range(0f,1f), Random.Range(0f, 1f), Random.Range(0f, 1f), 1f);
+
+            }
+        }
     }
 
     private void FixedUpdate()
